fccda60867e5e01841ac0d01f8895bc3f8d825ed
Alvmo1 <moralesalv101@hotmail.com>
2024-04-19 14:58:00 -0400

    2 commit

---
+++ src/test/java/assessment2/PCTest.java
@@ -0,0 +1,17 @@
+package assessment2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PCTest {
+
+    @Test
+    void description() {
+        PC pc = new PC
+    }
+
+    @Test
+    void powerUp() {
+    }
+}
